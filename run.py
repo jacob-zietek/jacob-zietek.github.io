091ae80ab828913c73ec8944f8448229f312eaa2
jacob-zietek <xoqcpn@gmail.com>
2022-02-17 09:53:49 -0500

    Pushing to repo

--- run.py
+++ run.py
@@ -15,9 +15,17 @@ for f in markdown_files: index_lines.append(f' - {f}')
 
 print(index_lines)
 
-
-
 # Code to fix include statements in _config.yml 
+
+config = open("_config.yml", "r")
+
+config_lines = config.read().split('\n')
+
+config_lines = config_lines[:config_lines.index("include:")+1]
+
+for f in markdown_files: config_lines.append(f' - {f}')
+
+print(config_lines)
 
 os.system('git add .')
 os.system('git commit -a -m "Pushing to repo"')
