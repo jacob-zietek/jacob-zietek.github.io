3e6d5bdeecc43766f1b3c9a243897ad8a47b9b69
jacob-zietek <xoqcpn@gmail.com>
2022-02-17 09:50:52 -0500

    Pushing to repo

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ index_lines = index.read().split('\n')
 
 print(f"Fixing: ... {index_lines}")
 
-index_lines = index_lines[:index_lines.index("Links:")]
+index_lines = index_lines[:index_lines.index("Links:")+1]
 
 for f in markdown_files: index_lines.append(f' - {f}')
 
